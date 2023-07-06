735a067ef2b30b00c1c7629b2568f72b2c08a59d
Andrey <mr.kuzyushin@yandex.ru>
2023-07-06 23:29:33 +0500

    third task

---
+++ Task03/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double [] InitArray(int dimension)
+{
+    double[] array = new double [dimension];
+    Random rnd = new Random();
+    for (int i = 0; i < dimension; i++)
+    {
+        array[i] = rnd.Next(1,100);
+    }
+
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+double GetMax(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    return max;
+}
+
+double GetMin(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    return min;
+}
+
+double[] arr = InitArray(5);
+PrintArray(arr);
+
+double max = GetMax(arr);
+double min = GetMin(arr);
+
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {max-min}");
